8b957f56eaa671ce388aee643c1a82d1144a9e7a
theinfin1ty <anantgaurav1010@gmail.com>
2024-01-01 22:27:45 +0530

    feat: added proccessCsv and Test_processCsv

--- csv-to-json.go
+++ csv-to-json.go
@@ -1,16 +1,30 @@
 package main
 
 import (
+	"encoding/csv"
 	"errors"
 	"flag"
 	"fmt"
+	"io"
 	"os"
+	"path/filepath"
 )
 
 type InputFile struct {
 	filePath  string
 	separator string
 	pretty    bool
+}
+
+func exitGracefully(err error) {
+	fmt.Fprintf(os.Stderr, "Error: %s\n", err)
+	os.Exit(1)
+}
+
+func check(e error) {
+	if e != nil {
+		exitGracefully(e)
+	}
 }
 
 func getFileData() (InputFile, error) {
@@ -30,6 +44,78 @@ func getFileData() (InputFile, error) {
 		separator: *separator,
 		pretty:    *pretty,
 	}, nil
+}
+
+func checkIfValidFile(filename string) (bool, error) {
+	fileExtension := filepath.Ext(filename)
+
+	if fileExtension != ".csv" {
+		return false, errors.New("File extension is not valid")
+	}
+
+	_, err := os.Stat(filename)
+
+	if err != nil && os.IsNotExist(err) {
+		return false, errors.New("File does not exist")
+	}
+
+	return true, nil
+}
+
+func processLine(headers []string, dataList []string) (map[string]string, error) {
+	if len(dataList) != len(headers) {
+		return nil, errors.New("Line doesn't match headers format. Skipping")
+	}
+
+	recordMap := make(map[string]string)
+
+	for i, name := range headers {
+		recordMap[name] = dataList[i]
+	}
+
+	return recordMap, nil
+}
+
+func processCsvFile(fileData InputFile, writerChannel chan<- map[string]string) {
+	file, err := os.Open(fileData.filePath)
+
+	check(err)
+
+	defer file.Close()
+
+	// var headers, line []string
+
+	reader := csv.NewReader(file)
+
+	if fileData.separator == "" {
+		fileData.separator = ","
+	}
+
+	reader.Comma = rune(fileData.separator[0])
+
+	headers, err := reader.Read()
+
+	check(err)
+
+	for {
+		line, err := reader.Read()
+
+		if err == io.EOF {
+			close(writerChannel)
+			break
+		} else if err != nil {
+			exitGracefully(err)
+		}
+
+		record, err := processLine(headers, line)
+
+		if err != nil {
+			fmt.Printf("Line: %sError: %s\n", line, err)
+			continue
+		}
+
+		writerChannel <- record
+	}
 }
 
 func main() {
--- csv-to-json_test.go
+++ csv-to-json_test.go
@@ -7,15 +7,28 @@ import (
 	"testing"
 )
 
-type Test struct {
+type FileDataTest struct {
 	name    string
 	want    InputFile
 	wantErr bool
 	osArgs  []string
 }
 
+type ValidFileTest struct {
+	name     string
+	filename string
+	want     bool
+	wantErr  bool
+}
+
+type ProcessCsvFileTest struct {
+	name      string
+	csvString string
+	separator string
+}
+
 func Test_getFileData(t *testing.T) {
-	tests := []Test{
+	tests := []FileDataTest{
 		{
 			name: "Default parameters",
 			want: InputFile{
@@ -84,6 +97,94 @@ func Test_getFileData(t *testing.T) {
 
 			if !reflect.DeepEqual(got, test.want) {
 				t.Errorf("getFileData() = %v, want %v", got, test.want)
+			}
+		})
+	}
+}
+
+func Test_checkIfValidFile(t *testing.T) {
+	tmpFile, err := os.CreateTemp("", "test.csv")
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer os.Remove(tmpFile.Name())
+
+	tests := []ValidFileTest{
+		{
+			name:     "File does exist",
+			filename: tmpFile.Name(),
+			want:     true,
+			wantErr:  false,
+		},
+		{
+			name:     "File does not exist",
+			filename: "nowhere/test.csv",
+			want:     false,
+			wantErr:  true,
+		},
+		{
+			name:     "File is not csv",
+			filename: "test.txt",
+			want:     false,
+			wantErr:  true,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			got, err := checkIfValidFile(test.filename)
+
+			if err != nil && !test.wantErr {
+				t.Errorf("checkIfValidFile() error = %v, wantErr %v", err, test.wantErr)
+				return
+			}
+
+			if got != test.want {
+				t.Errorf("checkIfValidFile() = %v, want %v", got, test.want)
+				return
+			}
+		})
+	}
+}
+
+func Test_processCsvFile(t *testing.T) {
+	wantMapSlice := []map[string]string{
+		{"COL1": "1", "COL2": "2", "COL3": "3"},
+		{"COL1": "4", "COL2": "5", "COL3": "6"},
+	}
+
+	tests := []ProcessCsvFileTest{
+		{"Comma separator", "COL1,COL2,COL3\n1,2,3\n4,5,6\n", ","},
+		{"Semicolon separator", "COL1;COL2;COL3\n1;2;3\n4;5;6\n", ";"},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			tmpfile, err := os.CreateTemp("", "test.csv")
+			check(err)
+			defer os.Remove(tmpfile.Name())
+
+			_, err = tmpfile.WriteString(test.csvString)
+			tmpfile.Sync()
+
+			testFileData := InputFile{
+				filePath:  tmpfile.Name(),
+				pretty:    false,
+				separator: test.separator,
+			}
+
+			writerChannel := make(chan map[string]string)
+
+			go processCsvFile(testFileData, writerChannel)
+
+			for _, wantMap := range wantMapSlice {
+				record := <-writerChannel
+				if !reflect.DeepEqual(record, wantMap) {
+					t.Errorf("processCsvFile() = %v, want %v", record, wantMap)
+					return
+				}
 			}
 		})
 	}
